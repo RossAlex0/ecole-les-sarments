bd9aab770198e0e8d9e476fbb67a1c962b51ca1d
Rossignol Alex <alexnw33910@gmail.com>
2026-05-24 17:29:39 +0200

    refactor: improve button component

--- src/components/ui/sarmentsButton/SarmentsButton.tsx
+++ src/components/ui/sarmentsButton/SarmentsButton.tsx
@@ -3,14 +3,29 @@ import SarmentsText from "../sarmentsText/SarmentsText";
 import "./sarmentsButton.css";
 
 export type CustomButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  theme?: "dark" | "light";
+  hasBorder?: boolean;
   children: React.ReactNode;
   href?: string;
 };
 
-export default function SarmentsButton({ children, href = "/", ...props }: CustomButtonProps) {
+export default function SarmentsButton({
+  theme,
+  hasBorder,
+  children,
+  href = "/",
+  ...props
+}: CustomButtonProps) {
+  const customClass = `${theme === "dark" ? "dark_btn" : "light_btn"} ${hasBorder ? "border" : ""}`;
+
   return (
     <Link href={href}>
-      <button type="button" {...props} className="button_custom" aria-label="button">
+      <button
+        type="button"
+        {...props}
+        className={`button_custom ${customClass} ${props.className}`}
+        aria-label="button"
+      >
         <SarmentsText format="view">{children}</SarmentsText>
       </button>
     </Link>
