a4b867df551dac1a086dc429ae2f2e0bb63f03da
Rossignol Alex <alexnw33910@gmail.com>
2026-05-24 17:28:27 +0200

    feat: add citation banner component

---
+++ src/components/block/citation-banner/CitationBanner.tsx
@@ -0,0 +1,21 @@
+import Quote from "@/components/ui/quote/Quote";
+import SarmentsText from "@/components/ui/sarmentsText/SarmentsText";
+
+import "./citationBanner.css";
+
+export default function CitationBanner() {
+  return (
+    <div className="citation_banner">
+      <Quote className="first_quote" />
+      <div className="citation_banner_title">
+        <SarmentsText format="fat-title">Connaitre pour aimer, aimer pour connaitre</SarmentsText>
+        <SarmentsText format="text" className="citation_banner_title_desc">
+          Transmettre l’excellence et former chaque enfant dans toutes ses dimensions
+          intellectuelles et humaines : telle est la vocation des Sarments où chaque élève découvre,
+          s’épanouit et rayonne.
+        </SarmentsText>
+      </div>
+      <Quote className="last_quote" />
+    </div>
+  );
+}
