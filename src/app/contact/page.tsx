eb09a3c1b4f4233dca55b5fdaab2539cda3b55ba
Rossignol Alex <alexnw33910@gmail.com>
2026-05-24 17:53:45 +0200

    feat(route-app): init route of project

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,7 @@
+export default function ContactPage() {
+  return (
+    <section style={{ height: "86vh", paddingTop: "14vh" }}>
+      <p>hello</p>
+    </section>
+  );
+}
---
+++ src/app/school/page.tsx
@@ -0,0 +1,7 @@
+export default function SchoolPage() {
+  return (
+    <section style={{ height: "86vh", paddingTop: "14vh" }}>
+      <p>hello</p>
+    </section>
+  );
+}
